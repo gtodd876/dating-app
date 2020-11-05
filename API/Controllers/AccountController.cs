93eb164600204c685dcdcd1a81b0cbdfb5b69d02
Todd Matthews <v-Todd_Matthews@carmax.com>
2020-11-05 14:16:42 -0500

    End of section 8

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -29,7 +29,7 @@ namespace API.Controllers
 
       var user = new AppUser
       {
-        UserName = registerDto.Username.ToLower(),
+        Username = registerDto.Username.ToLower(),
         PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(registerDto.Password)),
         PasswordSalt = hmac.Key
       };
@@ -39,7 +39,7 @@ namespace API.Controllers
 
       return new UserDto
       {
-        Username = user.UserName,
+        Username = user.Username,
         Token = _tokenService.CreateToken(user)
       };
     }
@@ -47,7 +47,7 @@ namespace API.Controllers
     [HttpPost("login")]
     public async Task<ActionResult<UserDto>> Login(LoginDto loginDto)
     {
-      var user = await _context.Users.SingleOrDefaultAsync(u => u.UserName.ToLower() == loginDto.Username.ToLower());
+      var user = await _context.Users.SingleOrDefaultAsync(u => u.Username.ToLower() == loginDto.Username.ToLower());
 
       if (user == null) return Unauthorized("Invalid username");
 
@@ -62,14 +62,14 @@ namespace API.Controllers
 
       return new UserDto
       {
-        Username = user.UserName,
+        Username = user.Username,
         Token = _tokenService.CreateToken(user)
       };
     }
 
     private async Task<bool> UserExists(string username)
     {
-      return await _context.Users.AnyAsync(x => x.UserName == username.ToLower());
+      return await _context.Users.AnyAsync(x => x.Username == username.ToLower());
     }
   }
 }
